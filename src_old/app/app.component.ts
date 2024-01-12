76cdbdd2c75c3d677a40f5c051ede93ef7c88b6f
augustobrigo <profeaugustobriga@gmail.com>
2024-01-12 08:51:44 +0100

    ListadoCochesOK

--- src_old/app/app.component.ts
+++ src_old/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import {MatTableDataSource, MatTableModule} from '@angular/material/table';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listadocoches';
+  title = 'listadoCoches';
 }
